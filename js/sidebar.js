fc859ac0bbf03ed3e76ed03c10dfea4a691dc92c
Robert Ziesenis <robert@robertziesenis.xyz>
2026-05-09 22:41:53 +0200

    Implemented context sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,16 @@
+const sidebar = document.querySelector(".sidebar-container");
+sidebar.setAttribute("icon-data", "←");
+
 function toggleSidebar() {
-  const sidebar = document.querySelector(".sidebar-container");
   sidebar.classList.toggle("show");
+
+  if (sidebar.classList.contains("show")) {
+    sidebar.setAttribute("icon-data", "→");
+  } else {
+    sidebar.setAttribute("icon-data", "←");
+  }
 }
+
+const currentYearCopyright = document.getElementById("current-year-copyright");
+const currentYear = new Date().getFullYear();
+currentYearCopyright.innerHTML = `© ${currentYear}`;
